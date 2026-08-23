9bed3c1d5a36985921e94650fa9ff8c08dfea95c
Abhinandh S <abhinandhsuby@proton.me>
2026-08-23 20:41:52 +0530

    Delete theme-pre.js [skip ci]

--- src/js/theme-pre.js
+++
@@ -1,9 +0,0 @@
-let savedTheme = localStorage.getItem('theme');
-  if (!savedTheme) {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    savedTheme = prefersDark ? 'dark' : 'light';
-  } 
-
-document.documentElement.setAttribute('data-theme', savedTheme);
-
-console.log(`[theme]: set to ${savedTheme}`)
