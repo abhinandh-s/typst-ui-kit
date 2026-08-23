c21d36d4d756c514437b35f8671879df05aecedb
Abhinandh S <abhinandhsuby@proton.me>
2026-08-23 16:44:47 +0530

    Update theme-pre.js

--- src/js/theme-pre.js
+++ src/js/theme-pre.js
@@ -1,6 +1,9 @@
-  let savedTheme = localStorage.getItem('theme');
+let savedTheme = localStorage.getItem('theme');
   if (!savedTheme) {
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     savedTheme = prefersDark ? 'dark' : 'light';
   } 
-  document.documentElement.setAttribute('data-theme', savedTheme);
+
+document.documentElement.setAttribute('data-theme', savedTheme);
+
+console.log(`[theme]: set to ${savedTheme}`)
