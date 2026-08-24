eeda42615ffccaf5996fcc9702e2080cad1f5042
Abhinandh S <abhinandhsuby@proton.me>
2026-08-24 13:28:08 +0530

    Create fixes.js

---
+++ src/js/fixes.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('.bottom-nav');
+    const endnotes = document.querySelector('section[role=\"doc-endnotes\"]');
+    
+    if (endnotes) {
+      const divider = document.createElement('hr');
+      divider.className = 'footnotes-divider';
+      endnotes.insertAdjacentElement('beforebegin', divider);
+      
+      if (nav) {
+        endnotes.insertAdjacentElement('afterend', nav);
+      }
+    } else if (nav) {
+      document.body.appendChild(nav);
+    }
+  });
